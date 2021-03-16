6fd3bea77d75487be55e0e414aa4b986b4f7e946
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-15 21:41:55 -0700

    reformat date and time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,5 @@
-var currentDate = document.querySelector("#today")
+var displayCurrentDate = document.querySelector("#today")
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-
-currentDate.textContent = today;
-
-// option using moment.js
-
-var displayCurrentDay = document.querySelector("#format")
-
-var currentDay = moment();
-displayCurrentDay.textContent = currentDay.format("dddd, MMMM Do YYYY LT");
+var currentDate = moment();
+displayCurrentDate.textContent = currentDate.format("dddd, MMMM Do YYYY LT");
   
