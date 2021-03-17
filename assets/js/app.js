00947584b16f4263a4d68ea20acf2a6d12ead898
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-17 13:53:29 -0700

    states dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,10 +11,80 @@ var humidity = document.querySelector(".humidity");
 var wind = document.querySelector(".wind");
 var uvi = document.querySelector(".uvi");
 
+var selectElement = document.getElementById("states");
+var states = [
+  "AL",
+  "AK",
+  "AS",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "DC",
+  "FM",
+  "FL",
+  "GA",
+  "GU",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MH",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "MP",
+  "OH",
+  "OK",
+  "OR",
+  "PW",
+  "PA",
+  "PR",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VI",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
+];
+
 // date and time
 var displayCurrentDate = document.querySelector("#today");
 var currentDate = moment();
 displayCurrentDate.textContent = currentDate.format("dddd, MMMM Do YYYY");
+
+for (var i = 0; i < states.length; i++) {
+  let newOption = document.createElement("option");
+  newOption.value = states[i];
+  newOption.innerText = states[i];
+  selectElement.appendChild(newOption);
+}
 
 searchBtn.addEventListener("click", function () {
   var apiUrl =
@@ -54,6 +124,7 @@ searchBtn.addEventListener("click", function () {
     .catch(function (error) {
       alert("Unable to connect to OpenWeatherMap");
     });
+
   $.ajax({
     type: "GET",
     url:
